6190fea3631b81ed1ee3056343814ba5532c9a91
Florian Esslinger <florian.esslinger@live.Fr>
2017-12-11 23:11:26 +0100

    Suite outliner

--- src/components/Outliner/Outliner.js
+++ src/components/Outliner/Outliner.js
@@ -1,15 +1,8 @@
 import React from "react";
 import Hypertopic from 'hypertopic';
 import conf from '../../config/config.json';
-//mettre en state DONE
-//terme en anglais DONE
-//fetch hypertopic PRESQUE
-//intégrer src (fork/clone)
-//gérer URI (en pointant)
-//route utiliser ee app dans src
-//mettre en 2eme ligne /viewpoint/:id
-//component Outliner
-//mettre en formulaire (<input>)
+const db = new Hypertopic(conf.services);
+
 //modifier feuille de style
 
 class Outliner extends React.Component {
@@ -17,91 +10,117 @@ class Outliner extends React.Component {
     super();
     this.state = {};
   }
+
   render() {
     return (
       <div>
-        <h1> {this.state.title} </h1>
-          <ul>
-            <Tree data={this.state} father={this.state.upper}/>
-          </ul>
+        <h1>
+          {this.state.title}
+        </h1>
+        <Tree data={this.state} father={this.state.upper} id={this.props.match.params.id}/>
       </div>
     );
   }
 
   componentDidMount() {
-    console.log("on fetch la data");
     this._fetchData();
-    console.log("on set upper");
-    console.log("on lance timer");
-    this.timerID = setInterval(
-     () => this.tick(),
-     1000
-   );
- }
-
- componentWillUnmount() {
-   clearInterval(this.timerID);
- }
-
- tick() {
-   if (this.state.fathers.upper !== this.state.upper){
-     console.log("le upper change");
-   this.setState({
-     upper: this.state.fathers.upper
-   });
+    this._timer = setInterval(
+      () => {
+        if (this.state.fathers.upper !== this.state.upper){
+          this.setState({
+            upper: this.state.fathers.upper
+          })
+        }
+      },
+      1000
+    );
   }
- }
+
+  componentWillUnmount() {
+    clearInterval(this._timer);
+  }
 
   _fetchData() {
 
-        const db = new Hypertopic(conf.services);
+    var listFatherGroup = {};
 
-        var listFatherGroup = {};
-
-        db.get({ _id: this.props.match.params.id })
-          .then(x => {this.setState({topics : x.topics});
-                this.setState({title : x.viewpoint_name});
-                return x.topics})
-          .then(x => {
-            let listFather = Object.keys(x).map(
-              k =>
-                (k = x[k].broader.length < 1 ? { upper: [k] } : { [x[k].broader]: [k] })
-            );
-            listFather.forEach(
-              e =>
-                typeof listFatherGroup[Object.keys(e)] === "undefined"
-                  ? (listFatherGroup[Object.keys(e)] = Object.values(e)[0])
-                  : (listFatherGroup[Object.keys(e)] = [
-                    ...listFatherGroup[Object.keys(e)],
-                    ...Object.values(e)[0]
-                  ])
-            );
-            console.log(listFatherGroup);
-            return listFatherGroup;
-          });
-          this.setState({fathers : listFatherGroup});
+    db.get({ _id: this.props.match.params.id })
+    .then(x => {this.setState({topics : x.topics});
+    this.setState({title : x.viewpoint_name});
+    return x.topics})
+    .then(x => {
+      let listFather = Object.keys(x).map(
+        k =>
+        (k = x[k].broader.length < 1 ? { upper: [k] } : { [x[k].broader]: [k] })
+      );
+      listFather.forEach(
+        e =>
+        typeof listFatherGroup[Object.keys(e)] === "undefined"
+        ? (listFatherGroup[Object.keys(e)] = Object.values(e)[0])
+        : (listFatherGroup[Object.keys(e)] = [
+          ...listFatherGroup[Object.keys(e)],
+          ...Object.values(e)[0]
+        ])
+      );
+      console.log(listFatherGroup);
+      return listFatherGroup;
+    });
+    this.setState({fathers : listFatherGroup});
   }
 
 }
 
 class Tree extends React.Component {
-    render() {
-      if (Array.isArray(this.props.father)) {
-        const peres = this.props.father.map( e => (typeof this.props.data.fathers[e] === "object") ?
-            <li> {this.props.data.topics[e].name} <ul> {this._getFils(e)} </ul></li> :
-            <li> {this.props.data.topics[e].name} </li>
-        );
-          return (<div> {peres} </div>);
-      } else {
-        return <li> NOTHING </li>
-      }
-    }
 
-    _getFils(e) {
-      return (
-            <Tree father={this.props.data.fathers[e]} data={this.props.data} />
-      );
+  constructor(props) {
+    super(props);
+    this.state = {childs : []};
+  }
+
+  handleChange(index,event) {
+    const _log = (x) => console.log(JSON.stringify(x, null, 2));
+    var childs = this.state.childs.slice();
+    childs[index] = event.target.value;
+    this.setState({childs: childs});
+    db.get({_id:this.props.id})
+    .then(x => Object.assign(x.topics[this.props.father[index]], {name:childs[index]}))
+    .then(db.post);
+
+    db.get({_id:this.props.id})
+    .then(x => x = x.topics[this.props.father[index]])
+    .then(_log);
+
+    console.log(db.post);
+  }
+
+  render() {
+    if (Array.isArray(this.props.father)) {
+      const childs = this.props.father.map((e,idx) =>
+      (typeof this.props.data.fathers[e] === "object") ?
+      (this.state.childs.push(this.props.data.topics[e].name),
+      //<form onSubmit={this.handleSubmit}>
+        <li key={e}>
+          <input type="text" value={this.state.childs[idx]}  onChange={this.handleChange.bind(this,idx)} />
+          <ul> {this._getChilds(e)} </ul>
+        </li>
+      //</form>
+    )
+      :
+      (this.state.childs.push(this.props.data.topics[e].name),
+      <li key={e}>
+        <input type="text" value={this.state.childs[idx]}  onChange={this.handleChange.bind(this,idx)} />
+      </li>));
+      return (<ul> {childs} </ul>);
+    } else {
+      return <li> NOTHING </li>
     }
+  }
+
+  _getChilds(e) {
+    return (
+      <Tree father={this.props.data.fathers[e]} data={this.props.data} id={this.props.id}/>
+    );
+  }
 }
 
 export default Outliner;
